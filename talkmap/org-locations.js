7c024cbb743964090019dcd35e167ab188177091
Tyrone Chen <chen.tyrone91@gmail.com>
2020-11-13 18:48:07 +1100

    Generate talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,32 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
+    "Cambridge, United Kingdom", 
+    52.2034823, 
+    0.1235817
+  ], 
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
+    "Canberra, Australia", 
+    -35.2975906, 
+    149.1012676
+  ], 
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
+    "Montreal, Canada", 
+    45.4972159, 
+    -73.6103642
+  ], 
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Melbourne, Australia", 
+    -37.8142176, 
+    144.9631608
+  ], 
+  [
+    "Heidelberg, Germany", 
+    49.4093582, 
+    8.694724
+  ], 
+  [
+    "Sydney, Australia", 
+    -33.8548157, 
+    151.2164539
   ]
 ];
